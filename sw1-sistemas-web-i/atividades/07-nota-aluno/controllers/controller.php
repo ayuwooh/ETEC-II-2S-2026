540476fb928f94272dba3aa5a732f6b9f9f8f920
ayuwooh <ayuwooh@gmail.com>
2026-09-08 12:12:54 -0300

    media nota em php

---
+++ sw1-sistemas-web-i/atividades/07-nota-aluno/controllers/controller.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/../models/aluno.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'] ?? '';
+    $nota1 = $_POST['nota1'] ?? 0;
+    $nota2 = $_POST['nota2'] ?? 0;
+    $nota3 = $_POST['nota3'] ?? 0;
+    $faltas = $_POST['faltas'] ?? 0;
+
+    $aluno = new Aluno();
+    $aluno->setNome($nome);
+    $aluno->setFaltas($faltas);
+    $aluno->calcularMedia($nota1, $nota2, $nota3);
+    $presenca = $aluno->calcularPresenca();
+    $resultado = $aluno->mostrarResultado();
+}
---
+++ sw1-sistemas-web-i/atividades/07-nota-aluno/models/aluno.php
@@ -0,0 +1,53 @@
+<?php
+
+class Aluno {
+    private $nome;
+    private $nota1, $nota2, $nota3;
+    private $faltas;
+    private $media;
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setFaltas($faltas) {
+        $this->faltas = $faltas;
+    }
+
+    public function calcularMedia($nota1, $nota2, $nota3) {
+        $this->nota1 = $nota1;
+        $this->nota2 = $nota2;
+        $this->nota3 = $nota3;
+        $this->media = ($nota1 + $nota2 + $nota3) / 3;
+        return $this->media;
+    }
+
+    public function getMedia() {
+        return $this->media;
+    }
+
+    public function calcularPresenca() {
+        $presenca = ((80 - $this->faltas) / 80) * 100;
+        return $presenca;
+    }
+
+    public function mostrarResultado() {
+        $presenca = $this->calcularPresenca();
+
+        if ($presenca < 75) {
+            return "Reprovado por falta";
+        }
+
+        if ($this->media < 5) {
+            return "Reprovado";
+        } elseif ($this->media >= 5 && $this->media <= 7) {
+            return "Recuperação";
+        } else {
+            return "Aprovado";
+        }
+    }
+}
