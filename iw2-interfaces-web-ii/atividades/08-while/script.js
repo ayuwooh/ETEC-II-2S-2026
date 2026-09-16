52f7eb34b94fa6ba8257aba11fa21ef3e2ce406f
ayuwooh <ayuwooh@gmail.com>
2026-09-15 23:32:03 -0300

    atv while

---
+++ iw2-interfaces-web-ii/atividades/08-while/script.js
@@ -0,0 +1,8 @@
+document.getElementById('submit').addEventListener("click", () => {
+    let userInput = Number(document.getElementById('userinput').value);
+    let count = userInput;
+    while (count > 0) {
+        document.writeln(count, "<br>");
+        count-=1;
+    }
+});
